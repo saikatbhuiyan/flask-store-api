dd4490263cd1a8c1b49b769b3a1b37c824d059d3
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-10-20 23:29:33 +0600

    simple api for item create and get

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
-from flask import Flask
+from flask import Flask, request
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def home():
-  return "Hello, world"
+items = []
 
-app.run(port=5000)
+class Item(Resource):
+  def get(self, name):
+    item = next(filter(lambda x: x['name'] == name, items), None)
+    return {'item': item}, 200 if item else 404
+
+  def post(self, name):
+    if next(filter(lambda x: x['name'] == name, items), None) is not None:
+      return {'message': "Item already exists with this {}".fomat(name)}, 400
+
+    data = request.get_json() #force=True, silent=True
+    item = {'name': name, 'price': data['price']}
+    items.append(item)
+    return item, 201
+
+class ItemList(Resource):
+  def get(self):
+    return {'items': items}
+
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)
