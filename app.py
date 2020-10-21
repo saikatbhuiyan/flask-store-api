556bbe8c77f1805d5cadc7f20751c0e38f658a7e
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-10-21 12:40:17 +0600

    simple jwt authentication

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'sami'
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
 
 items = []
 
 class Item(Resource):
+  @jwt_required()
   def get(self, name):
     item = next(filter(lambda x: x['name'] == name, items), None)
     return {'item': item}, 200 if item else 404
