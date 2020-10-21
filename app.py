7ec6fd4ba786bb5175539869ee7c12c3c7ad2676
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-10-21 13:07:20 +0600

    simple put delete item

--- app.py
+++ app.py
@@ -26,6 +26,24 @@ class Item(Resource):
     item = {'name': name, 'price': data['price']}
     items.append(item)
     return item, 201
+    
+  @jwt_required()
+  def delete(self, name):
+    global items
+    items = list(filter(lambda x: x['name'] !=name, items))
+    return {'message': 'Item deleted'}
+
+  @jwt_required()
+  def put(self, name):
+    data = Item.parser.parse_args()
+    # Once again, print something not in the args to verify everything works
+    item = next(filter(lambda x: x['name'] == name, items), None)
+    if item is None:
+      item = {'name': name, 'price': data['price']}
+      items.append(item)
+    else:
+      item.update(data)
+    return item
 
 class ItemList(Resource):
   def get(self):
