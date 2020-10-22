9a3ab7cf63a989091015a450a41f8324cffbdb05
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-10-22 22:00:58 +0600

    create db

---
+++ test.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+connection = sqlite3.connect('data.db')
+
+cursor = connection.cursor()
+
+# create_table = "CREATE TABLE users (id int, username text, password text)"
+
+# cursor.execute(create_table)
+
+# user = (1, 'sami', 'sami1234')
+insert_query = "INSERT INTO users VALUES (?, ?, ?)"
+# cursor.execute(insert_query)
+
+users = [
+  (2, 'sajid', 'sami1234'),
+  (3, 'abrar', 'sami1234')
+]
+
+cursor.execute(insert_query, users)
+
+
+select_query = "SELECT * FROM users"
+
+for row in cursor.execute(select_query):
+  print(row)
+
+
+connection.commit()
+
+connection.close()
