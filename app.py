3761dc752e5b205f96839e6a3d9f880d97013b47
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-10-24 23:59:29 +0600

    create item

--- app.py
+++ app.py
@@ -19,4 +19,5 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
-app.run(port=5000, debug=True)
+if __name__ == '__main__':
+  app.run(port=5000, debug=True)
--- item.py
+++ item.py
@@ -15,6 +15,15 @@ class Item(Resource):
 
     @jwt_required()
     def get(self, name):
+        item = self.find_by_name(name)
+
+        if item:
+            return item
+
+        return {'message': 'Item not found'} 404
+
+    @classmethod
+    def find_by_name(cls, name):
       connection = sqlite3.connect('data.db')
       cursor = connection.cursor()
 
@@ -25,17 +34,25 @@ class Item(Resource):
 
       if row:
         return {'item': {'name': row[0], 'price':row[1]}}
-      return {'message': 'Item not found'} 404
+
 
     def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items), None) is not None:
-            return {'message': "An item with name '{}' already exists.".format(name)}
+        if self.find_by_name(name):
+            return {'message': "An item with name '{}' already exists.".format(name)}, 400
 
         data = Item.parser.parse_args()
 
         item = {'name': name, 'price': data['price']}
-        items.append(item)
-        return item
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query, (item['name'], item['price']))
+        
+        connection.commit()
+        connection.close()
+
+        return item, 201
 
     @jwt_required()
     def delete(self, name):
